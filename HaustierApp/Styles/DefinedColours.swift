de88d61d33d12f35719db0a68dad28d3bad29085
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2023-07-16 13:40:00 +0200

    'Custom Colours

---
+++ HaustierApp/Styles/DefinedColours.swift
@@ -0,0 +1,44 @@
+//
+//  DefinedColours.swift
+//  HaustierApp
+//
+//  Created by Mobile1 on 15.07.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct Custom_UI {
+        
+        let darkGreen = Color("DarkGreen")
+        let green = Color("Green")
+        let blueWhite = Color("BlueWhite")
+        let burntOrange = Color("BurntOrange")
+        let lightBeige = Color("LightBeige")
+        let lightOrange = Color("LightOrange")
+        
+    }
+    
+//    Farben in dem Schema angeben
+//    static let ui = Color.UI_E()
+//
+//    struct UI_O {
+//        //Button
+//        let buttonOn = Color("ButtonOnColor")
+//        let buttonOff = Color("ButtonOffColor")
+//        let streamingOn = Color("StreamingOnColor")
+//
+//        let buttonLabel = Color("ButtonLabelColor")
+//
+//        // Areas
+//        let background = Color("BackgroundColor")
+//        let areaBackground = Color("AreaBackgroundColor")
+//        let cardbackground = Color("CardBackgroundColor")
+//
+//        //Label
+//        let label = Color("LabelColor")
+//    }
+    
+}
